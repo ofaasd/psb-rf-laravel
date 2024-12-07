ed092b0f9add5e451185718aa9a02fe86b304f65
ofaasd <ofaasd@gmail.com>
2024-12-07 09:29:59 +0700

    adding valign=top

--- resources/views/psb/_form_cetak.blade.php
+++ resources/views/psb/_form_cetak.blade.php
@@ -72,8 +72,8 @@
         </tr> --}}
         <tr>
             <td valign="top" >Nama Lengkap</td>
-            <td colspan="3">{{$psb_peserta->nama}}</td>
-            <td rowspan="8">
+            <td valign="top" colspan="3">{{$psb_peserta->nama}}</td>
+            <td valign="top" rowspan="8">
                 @if(!empty($berkas->file_photo))
                     <img src="{{URL::to('')}}/assets/images/upload/foto_casan/{{$berkas->file_photo}}" width="150">
                 @endif
@@ -85,148 +85,148 @@
         </tr>
         <tr>
             <td valign="top">Nama Panggilan</td>
-            <td colspan="3">{{$psb_peserta->nama_panggilan ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="3">{{$psb_peserta->nama_panggilan ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Jenis Kelamin</td>
-            <td colspan="3">{{($psb_peserta->jenis_kelamin=='L')?'Laki-laki':'Perempuan'}}</td>
+            <td valign="top" colspan="3">{{($psb_peserta->jenis_kelamin=='L')?'Laki-laki':'Perempuan'}}</td>
         </tr>
         <tr>
             <td valign="top" width="15%">Tempat Tanggal Lahir</td>
-            <td width="15%">{{$psb_peserta->tempat_lahir}}, {{date('d-m-Y', strtotime($psb_peserta->tanggal_lahir))}}</td>
-            <td width="15%">Usia</td>
-            <td width="15%">{{$psb_peserta->usia_tahun ?? '<Kosong>'}} Tahun, {{$psb_peserta->usia_bulan ?? '<Kosong>'}} Bulan</td>
+            <td valign="top" width="15%">{{$psb_peserta->tempat_lahir}}, {{date('d-m-Y', strtotime($psb_peserta->tanggal_lahir))}}</td>
+            <td valign="top" width="15%">Usia</td>
+            <td valign="top" width="15%">{{$psb_peserta->usia_tahun ?? '<Kosong>'}} Tahun, {{$psb_peserta->usia_bulan ?? '<Kosong>'}} Bulan</td>
         </tr>
         <tr>
             <td valign="top">Anak Ke</td>
-            <td colspan="3">{{$psb_peserta->anak_ke ?? '<Kosong>'}} dari {{$psb_peserta->jumlah_saudara ?? '<Kosong>'}}  bersaudara</td>
+            <td valign="top" colspan="3">{{$psb_peserta->anak_ke ?? '<Kosong>'}} dari {{$psb_peserta->jumlah_saudara ?? '<Kosong>'}}  bersaudara</td>
         </tr>
         <tr>
             <td valign="top">Alamat Lengkap</td>
-            <td colspan="3">{{$psb_peserta->alamat ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="3">{{$psb_peserta->alamat ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Kelurahan</td>
-            <td>{{$kelurahan->nama_kelurahan ?? '<Kosong>'}}</td>
+            <td valign="top">{{$kelurahan->nama_kelurahan ?? '<Kosong>'}}</td>
             <td valign="top">Kecamatan</td>
-            <td >{{$kecamatan->nama_kecamatan ?? '<Kosong>'}}</td>
+            <td valign="top" >{{$kecamatan->nama_kecamatan ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Kota</td>
-            <td>{{$kota->nama_kota_kab ?? '<Kosong>'}}</td>
+            <td valign="top">{{$kota->nama_kota_kab ?? '<Kosong>'}}</td>
             <td valign="top">Provinsi</td>
-            <td>{{$provinsi->nama_provinsi ?? '<Kosong>'}}</td>
+            <td valign="top">{{$provinsi->nama_provinsi ?? '<Kosong>'}}</td>
         </tr>
         <tr>
-            <td colspan="5" ><b>Data Wali Santri</b></td>
+            <td colspan="5" valign="top"><b>Data Wali Santri</b></td>
         </tr>
         <tr>
             <td valign="top">Nama Ayah</td>
-            <td>{{$psb_wali->nama_ayah ?? '<Kosong>'}}</td>
+            <td valign="top">{{$psb_wali->nama_ayah ?? '<Kosong>'}}</td>
             <td valign="top">Nama Ibu</td>
-            <td colspan="2">{{$psb_wali->nama_ibu ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="2">{{$psb_wali->nama_ibu ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Pendidikan Ayah</td>
-            <td>{{$psb_wali->pendidikan_ayah ?? '<Kosong>'}}</td>
+            <td valign="top">{{$psb_wali->pendidikan_ayah ?? '<Kosong>'}}</td>
             <td valign="top">Pendidikan Ibu</td>
-            <td colspan="2">{{$psb_wali->pendidikan_ibu ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="2">{{$psb_wali->pendidikan_ibu ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Pekerjaan Ayah</td>
-            <td>{{$psb_wali->pekerjaan_ayah ?? '<Kosong>'}}</td>
+            <td valign="top">{{$psb_wali->pekerjaan_ayah ?? '<Kosong>'}}</td>
             <td valign="top">Pekerjaan Ibu</td>
-            <td colspan="2">{{$psb_wali->pekerjaan_ibu ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="2">{{$psb_wali->pekerjaan_ibu ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">No. HP</td>
-            <td>{{$psb_wali->no_hp}}</td>
+            <td valign="top">{{$psb_wali->no_hp}}</td>
             <td valign="top">Nomor Telpon Rumah</td>
-            <td colspan="4">{{$psb_wali->no_telp ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$psb_wali->no_telp ?? '<Kosong>'}}</td>
         </tr>
         <tr>
-            <td colspan="5" ><b>Sekolah Asal / Pindahan</b></td>
+            <td valign="top" colspan="5" ><b>Sekolah Asal / Pindahan</b></td>
         </tr>
         <tr>
             <td valign="top">Dari</td>
-            <td colspan="4">{{$jenjang[$psb_asal->jenjang] ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$jenjang[$psb_asal->jenjang] ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Nama Sekolah</td>
-            <td colspan="4">{{$psb_asal->nama_sekolah ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$psb_asal->nama_sekolah ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">NSM/NSS</td>
-            <td colspan="4">{{$psb_asal->nss ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$psb_asal->nss ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">NPSN</td>
-            <td colspan="4">{{$psb_asal->npsn ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$psb_asal->npsn ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">NISN</td>
-            <td colspan="4">{{$psb_asal->nisn ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$psb_asal->nisn ?? '<Kosong>'}}</td>
         </tr>
         <tr>
-            <td colspan="5" ><b>Ukuran Seragam</b></td>
+            <td valign="top" colspan="5" ><b>Ukuran Seragam</b></td>
         </tr>
         <tr>
             <td valign="top">Berat Badan</td>
-            <td colspan="4">{{$psb_seragam->berat_badan ?? '<Kosong>'}} KG</td>
+            <td valign="top" colspan="4">{{$psb_seragam->berat_badan ?? '<Kosong>'}} KG</td>
         </tr>
         <tr>
             <td valign="top">Tinggi Badan</td>
-            <td colspan="4">{{$psb_seragam->tinggi_badan ?? '<Kosong>'}} CM</td>
+            <td valign="top" colspan="4">{{$psb_seragam->tinggi_badan ?? '<Kosong>'}} CM</td>
         </tr>
         <tr>
             <td valign="top">Lingkar Dada</td>
-            <td colspan="4">{{$psb_seragam->lingkar_dada ?? '<Kosong>'}} CM</td>
+            <td valign="top" colspan="4">{{$psb_seragam->lingkar_dada ?? '<Kosong>'}} CM</td>
         </tr>
         <tr>
             <td valign="top">Lingkar Pinggul</td>
-            <td colspan="4">{{$psb_seragam->lingkar_pinggul ?? '<Kosong>'}} CM</td>
+            <td valign="top" colspan="4">{{$psb_seragam->lingkar_pinggul ?? '<Kosong>'}} CM</td>
         </tr>
         <tr>
-            <td colspan="5" ><b>Berkas Pendukung</b></td>
+            <td valign="top" colspan="5" ><b>Berkas Pendukung</b></td>
         </tr>
         <tr>
             <td valign="top">Photo Calon Santri</td>
-            <td colspan="4">{{(!empty($berkas->file_photo))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{(!empty($berkas->file_photo))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">File KK</td>
-            <td colspan="4">{{(!empty($berkas->file_kk))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{(!empty($berkas->file_kk))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">File KTP</td>
-            <td colspan="4">{{(!empty($berkas->file_ktp))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{(!empty($berkas->file_ktp))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">File Rapor/Ijazah</td>
-            <td colspan="4">{{(!empty($berkas->file_rapor))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{(!empty($berkas->file_rapor))? 'Ada (Terakhir Diperbaharui ' . date('d-m-Y',strtotime($berkas->updated_at)) . ')' : '<Kosong>'}}</td>
         </tr>
         <tr>
-            <td colspan="5" ><b>Status Pembayaran</b></td>
+            <td colspan="5" valign="top"><b>Status Pembayaran</b></td>
         </tr>
         <tr>
             <td valign="top">Nama Bank</td>
-            <td colspan="0">{{$bukti->bank ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="0">{{$bukti->bank ?? '<Kosong>'}}</td>
             <td valign="top">Bukti Pembayaran</td>
-            <td colspan="2">{{(!empty($bukti->bukti))? 'Ada' : '<Kosong>'}}</td>
+            <td valign="top" colspan="2">{{(!empty($bukti->bukti))? 'Ada' : '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">Atas Nama</td>
-            <td colspan="0">{{$bukti->atas_nama ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="0">{{$bukti->atas_nama ?? '<Kosong>'}}</td>
             <td valign="top">Status Pembayaran</td>
             @php $stats = 0; if(!empty($bukti->status)) $stats = $bukti->status @endphp
-            <td colspan="2">{{$status_pembayaran[$stats] ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="2">{{$status_pembayaran[$stats] ?? '<Kosong>'}}</td>
         </tr>
         <tr>
             <td valign="top">No. Rekening</td>
-            <td colspan="4">{{$bukti->no_rekening ?? '<Kosong>'}}</td>
+            <td valign="top" colspan="4">{{$bukti->no_rekening ?? '<Kosong>'}}</td>
         </tr>
         <tr>
-            <td colspan="5" style="background:red;color:white; padding:10px;">Digenerate Oleh Sistem PPATQ-RF.ID | Terakhir Di update {{date('Y-m-d H:i:s',strtotime($psb_peserta->updated_at))}}</td>
+            <td valign="top" colspan="5" style="background:red;color:white; padding:10px;">Digenerate Oleh Sistem PPATQ-RF.ID | Terakhir Di update {{date('Y-m-d H:i:s',strtotime($psb_peserta->updated_at))}}</td>
         </tr>
     </table>
 </body>
